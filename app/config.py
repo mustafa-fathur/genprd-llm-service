d732b6224d1d73cd81a7e01dc4c3be1b28399702
Fathur <fatur300904@gmail.com>
2025-05-22 20:15:26 +0700

    fix: update default port and host value

--- app/config.py
+++ app/config.py
@@ -6,8 +6,8 @@ class _Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env")
     GEMINI_API_KEY: str
     # OPENAI_API_KEY: str
-    PORT: str   
-    HOST: str  
+    PORT: str = "8080" 
+    HOST: str = "0.0.0.0"  
 
 
 dotenv.load_dotenv()
--- app/main.py
+++ app/main.py
@@ -37,7 +37,7 @@ def generate_prd():
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT'))
-    host = os.environ.get('HOST')
+    port = int(os.environ.get('PORT', 8080))
+    host = os.environ.get('HOST', '0.0.0.0')
     
     app.run(host=host, port=port, debug=False)
